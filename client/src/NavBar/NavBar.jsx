2c19977b8530706c8b8ebacf980869a91dbe4c62
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-23 15:15:35 -0800

    add Navbar

---
+++ client/src/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav className="w-screen h-1/4 bg-blue-900 p-5 text-white flex justify-between">
+      <div>
+        <Link to='/'>
+          <h1>myFitnessBuddy</h1>
+        </Link>
+      </div>
+      <div>
+        <Link to="/login">
+          <p>Login</p>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
