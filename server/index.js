401ab5c9df2192f5945b523e1918039837fa4529
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-25 10:00:11 -0800

    setup server and add auth route

--- server/index.js
+++ server/index.js
@@ -1,2 +1,34 @@
 import { Express } from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+const app = Express();
+
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "50mb", extened: true }));
+app.use(express.urlencoded({ limit: "50mb", extened: true }));
+dotenv.config();
+
+app.use("/auth", AuthRoute)
+
+mongoose
+  .connect(process.env.ATLAS_URI, {
+    dbName: 'myFitnessBuddy',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Listening on ${process.env.PORT}`);
+    })
+  )
+  .catch((error) => console.log(error));
+
